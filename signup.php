b64cf2fb5dc6ee0acf325e68f33a781955d051e0
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-10-03 12:06:29 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -10,12 +10,14 @@ if (isset($_POST['signup_submit'])) {
     $username = $_POST['username']
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $pass_confirm = $_POST['password_confirm'];
 
     $firstname = mysqli_real_escape_string($con, $firstname);
     $lastname = mysqli_real_escape_string($con, $lastname);
     $username = mysqli_real_escape_string($con, $username);
     $email = mysqli_real_escape_string($con, $email);
     $password = mysqli_real_escape_string($con, $password);
+    $password_confirm = mysqli_real_escape_string($con, $pass_confirm);
     
     //Validation
     //check if email exists
@@ -27,6 +29,9 @@ if (isset($_POST['signup_submit'])) {
     } else if (strlen($password) < 5) {
         addAlert('error', 'Password must be atleast Five (5) characters');
         echo "<script>document.location='signup.html'</script>";
+    } else if ($password != $confirm_password) {
+        addAlert('error', 'Passwords dont Match');
+        echo "<script>document.location='signup.html'</script>";
     } else if ($email_count > 0) {
         addAlert('error', 'Email address already exists!');
         echo "<script>document.location='signup.html'</script>";
@@ -34,13 +39,13 @@ if (isset($_POST['signup_submit'])) {
         $res = mysqli_query($con, "INSERT INTO users SET firstname = '$firstname', lastname = '$lastname', username = '$username', email = '$email', password = '$password',") or die(mysqli_error($con));
         if ($res) {
             addAlert('success', 'Registration Successful! Please Login');
-            echo "<script type='text/javascript'>document.location='index.html'</script>";
+            echo "<script type='text/javascript'>document.location='signup.html'</script>";
         } else {
             addAlert('error', 'Something went wrong!');
             echo "<script type='text/javascript'>document.location='signup.html'</script>";
         }
     }
 } else {
-    header('Location signup.html');
+    header('Location index.html');
 }
 ?>
